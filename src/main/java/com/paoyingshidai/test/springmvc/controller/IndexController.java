c45066acc7d3189237b2933d0e5a82adc7df2418
Michael <13432849094@163.com>
2019-10-28 21:57:42 +0800

    添加重定向

--- src/main/java/com/paoyingshidai/test/springmvc/controller/IndexController.java
+++ src/main/java/com/paoyingshidai/test/springmvc/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.paoyingshidai.test.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,6 +35,23 @@ public class IndexController {
         modelAndView.setViewName("index");
 
         return modelAndView;
+    }
+
+
+
+    // 重定向， 重定向传参
+    @RequestMapping("indexPageRedirect")
+    public String indexPageRedirect(ModelAndView modelAndView, RedirectAttributes attributes) {
+        attributes.addAttribute("test", "hello");
+        return "redirect:target";
+    }
+
+
+    @RequestMapping("target")
+    @ResponseBody
+    public String redirectTarget(@ModelAttribute("test") String test) {
+        System.out.println(test);
+        return test;
     }
 
 }
